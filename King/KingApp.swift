e6b64900e1b87c7fbd025bc6df8ccef6d869398d
sanjeevan43 <sangeevamoorthy35@gmail.com>
2025-06-13 22:56:32 +0530

    Initial Commit

---
+++ King/KingApp.swift
@@ -0,0 +1,17 @@
+//
+//  KingApp.swift
+//  King
+//
+//  Created by sanjeevan on 13/06/25.
+//
+
+import SwiftUI
+
+@main
+struct KingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
